fd1d05a2db696553c353ed54867e5a04a57a2f1f
EugeneYaremenko <yaremenko.e.s@gmail.com>
2020-07-13 19:14:40 +0300

    correct hw-12

--- src/js/services/fetchCountries.js
+++ src/js/services/fetchCountries.js
@@ -1,7 +1,14 @@
+const { alert, notice, info, success, error } = require('@pnotify/core');
 const baseUrl = 'https://restcountries.eu/rest/v2/name/';
 
 export default {
   fetchCountries(searchQuery) {
-    return fetch(baseUrl + searchQuery).then(responce => responce.json());
+    return fetch(baseUrl + searchQuery)
+      .then(responce => responce.json())
+      .catch(
+        error(
+          'The connection to the server is lost. Try using the service later.',
+        ),
+      );
   },
 };
